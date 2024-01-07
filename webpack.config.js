ca756af8ebd966350cf5f739aa6dba77ab68b324
Alessandra Patricio <alessandrapatricio18@gmail.com>
2024-01-07 12:16:27 -0300

    Atualização required, e rules webpack

--- webpack.config.js
+++ webpack.config.js
@@ -45,52 +45,21 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
+          {
+            loader: "css-loader",
+          },
         ],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-              name: '[name].[ext]',
-              outputPath: 'images/',
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              // Opções para otimização de imagens
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75
-              }
-            }
-          },
-        ],
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
